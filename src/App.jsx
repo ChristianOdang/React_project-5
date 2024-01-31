51089b91f7481f284aec154acd52910dbfd2e3db
JohnsonOdang <johnnyodanny@gmail.com>
2024-01-31 04:04:15 -0800

    handling multiple inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("");
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`The name you entered is: ${name}`)
-  }
+    alert(inputs);
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -14,12 +20,22 @@ function App() {
         Enter your name:
         <input
           type='text'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          name='username'
+          value={inputs.username || ""}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Enter your age:
+        <input
+          type='number'
+          name='age'
+          value={inputs.age || ""}
+          onChange={handleChange}
         />
       </label>
 
-      <input type="submit" />
+      <input type='submit' />
     </form>
   );
 }
