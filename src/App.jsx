d95b598f0eb28257eae47031cb48ab113af6c4e3
JohnsonOdang <johnnyodanny@gmail.com>
2024-01-31 15:48:51 -0800

    updating app

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,12 @@
-import React, { useState } from "react";
+import React from 'react'
+import MyForm from './MyForm'
 
 function App() {
-  const [inputs, setInputs] = useState({});
-  const [myCar, setMycar] = useState('Volvo')
-  const [textarea, setTextarea] = useState("Type your message here...")
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
-  }
-
-  const handleCar = (event) => {
-    setMycar(event.target.value)
-  }
-
-  const handleText = (event) => {
-    setTextarea(event.target.value)
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(inputs);
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Enter your name:
-        <input
-          type='text'
-          name='username'
-          value={inputs.username || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <br></br>
-      <label>
-        Enter your age:
-        <input
-          type='number'
-          name='age'
-          value={inputs.age || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <br></br>
-      <select value={myCar} onChange={handleCar}>
-        <option value="Ford">Ford</option>
-        <option value='Volvo'>Volvo</option>
-        <option value='Fiat'>Fiat</option>
-        <option value='Benz'>Benz</option>
-      </select>
-      <br></br>
-      <textarea value={textarea} onChange={handleText} />
-
-      <input type='submit' />
-    </form>
-  );
+    <>
+      <MyForm />
+    </>
+  )
 }
 
-export default App;
+export default App
